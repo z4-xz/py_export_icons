ba0bb82eea2f6bcd59e429d04b9be0f247761a3d
z4-xz <8pc8@protonmail.com>
2021-01-18 15:52:28 +0100

    Proper listing all .svg files in certain dir to txt file

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import os
 
 now_str = datetime.now().strftime("%d_%m_%Y_%H;%M")
 report_file = open("report_file_" + str(now_str) + ".txt", "a")
-report_file.write("Report file generated at " + now_str)
+report_file.write("Report file generated at " + now_str + "\n")
+files = os.listdir('./icons')
+for file in files:
+    if(file.endswith(".svg")):
+        report_file.write(file + "\n")
+
 report_file.close()
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-    print(f)
