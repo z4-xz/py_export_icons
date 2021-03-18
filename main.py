6d8edd93bc581fa0d65a1c5fdc1db8cdf8d1d289
z4-xz <8pc8@protonmail.com>
2021-03-18 13:18:22 +0100

    Code refactoring

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from PIL import Image
 from cairosvg import svg2png
 import argparse
 
+settings = json.load(open('settings.json', 'r')) 
+bg_color = settings["background"]
+input_dir_path = "./svg/"
 
 def concatenate(icons: list, path: str):
     """Paste images one by one into summary PNG file.
@@ -45,57 +48,54 @@ def concatenate(icons: list, path: str):
             in_column_counter += 1
     result_image.save("output.png", "PNG")
 
+def main():
+    global settings
+    parser = argparse.ArgumentParser(epilog = "### z4-xz.github.io ###")
+    parser.add_argument("-r", "--rows", 
+                        help = "Number of ROWS to draw in output file", 
+                        default = settings["rows"], 
+                        type = int)
+    parser.add_argument("-c", "--columns", 
+                        help = "Number of COLUMNS to draw in output file", 
+                        default = settings["columns"], 
+                        type = int)
+    parser.add_argument("-ih", "--iconheight", 
+                        help = "Single icon height in pixels", 
+                        default = settings["icon_height"], 
+                        type = int)
+    parser.add_argument("-iw", "--iconwidth", 
+                        help = "Single icon width in pixels", 
+                        default = settings["icon_width"], 
+                        type = int)
+    args_dict  = vars(parser.parse_args())
 
-settings = json.load(open('settings.json', 'r'))
-bg_color = settings["background"]
+    settings["columns"] = abs(args_dict["columns"])
+    settings["rows"] = abs(args_dict["rows"])
+    settings["icon_height"] = abs(args_dict["iconheight"])
+    settings["icon_width"] = abs(args_dict["iconwidth"])
+    output_dir_path = str(datetime.now().strftime("%d_%m_%Y_%H-%M-%S")) + "/"
+    os.mkdir(output_dir_path)
+    print("Directory {} created".format(output_dir_path))
+    svgs = []
+    pngs = []
+    svgs += [file for file in (os.listdir(input_dir_path)) if(file.endswith(".svg"))]
+    print("Listing...")
+    for svg in svgs:
+        try:
+            png = svg.replace(".svg", ".png")
+            svg2png(url = input_dir_path + str(svg), 
+                    write_to = output_dir_path + str(png), 
+                    output_width = settings["icon_width"], 
+                    output_height = settings["icon_height"])
+            pngs += [png]
+        except Exception:
+            print("Exception occured")
 
+    if len(pngs) <= settings["rows"] * settings["columns"]:
+        concatenate(pngs, output_dir_path)
+    else:
+        print("Not enough rows or cols")
+    print("Done.")
 
-parser = argparse.ArgumentParser(epilog = "### z4-xz.github.io ###")
-parser.add_argument("-r", "--rows", 
-                    help = "Number of ROWS to draw in output file", 
-                    default = settings["rows"], 
-                    type = int)
-parser.add_argument("-c", "--columns", 
-                    help = "Number of COLUMNS to draw in output file", 
-                    default = settings["columns"], 
-                    type = int)
-parser.add_argument("-ih", "--iconheight", 
-                    help = "Single icon height in pixels", 
-                    default = settings["icon_height"], 
-                    type = int)
-parser.add_argument("-iw", "--iconwidth", 
-                    help = "Single icon width in pixels", 
-                    default = settings["icon_width"], 
-                    type = int)
-args_dict  = vars(parser.parse_args())
-
-print(abs(args_dict["columns"]))
-settings["columns"] = abs(args_dict["columns"])
-print(abs(args_dict["rows"]))
-settings["rows"] = abs(args_dict["rows"])
-settings["icon_height"] = abs(args_dict["iconheight"])
-settings["icon_width"] = abs(args_dict["iconwidth"])
-print(concatenate.__doc__)
-input_dir_path = "./svg/"
-output_dir_path = str(datetime.now().strftime("%d_%m_%Y_%H-%M-%S")) + "/"
-os.mkdir(output_dir_path)
-svgs = []
-pngs = []
-svgs += [file for file in (os.listdir(input_dir_path)) if(file.endswith(".svg"))]
-
-for svg in svgs:
-    try:
-        png = svg.replace(".svg", ".png")
-        svg2png(url = input_dir_path + str(svg), 
-                write_to = output_dir_path + str(png), 
-                output_width = settings["icon_width"], 
-                output_height = settings["icon_height"])
-        pngs += [png]
-    except:
-        print("ERROR")
-
-if len(pngs) <= settings["rows"] * settings["columns"]:
-    concatenate(pngs, output_dir_path)
-else:
-    print("Not enough rows or cols")
-print("Done.")
+if __name__ == '__main__':
+    main()
